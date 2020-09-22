af71f30d94d92050329b012709377f6d566f4350
yangkun <yangkun@leying365.com>
2020-09-22 18:36:00 +0800

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
-  mode:'development',
-  entry:{
-    'main':'./src/main.js'
+  mode: 'development',
+  entry: {
+    'main': './src/main.js'
   },
-  output:{
-    path:path.resolve(__dirname,'dist'),
-    filename:'main.js'
-  }
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template:'./src/index.html'
+    })
+  ]
 }
-
