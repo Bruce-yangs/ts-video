944ab358ef75e1b92c26f9730e75837bfb794629
Bruce yangs <469402333@qq.com>
2020-10-13 00:29:34 +0800

    ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-import a from './a'
-import './a.css'
+import './main.css'
 import './iconfont/iconfont.css'
 
 // let a: string = "hello1";
 
-console.log(a);
+// console.log(a);
+
+let listItem = document.querySelectorAll('#list li');
+
+for(let i=0;i<listItem.length;i++) {
+    listItem[i].addEventListener('click',function(){
+        let url = this.dataset.url;
+        let title = this.dataset.title;
+        console.log(title,url)
+    })
+}
