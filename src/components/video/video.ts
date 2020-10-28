05b64df7c0ac09fd2f9e9d4bc328fae4f5561fc0
Bruce yangs <469402333@qq.com>
2020-10-29 00:31:11 +0800

    drag play video

--- src/components/video/video.ts
+++ src/components/video/video.ts
@@ -82,7 +82,11 @@ class Video implements Icomponent{
         let videoPlay= this.dom.querySelector(`.${styles['video-controls']} i`);
         let videoTimes= this.dom.querySelectorAll(`.${styles['video-time']} span`);
         let videoFull= this.dom.querySelector(`.${styles['video-full']} i`);
+        let videoProgress= this.dom.querySelectorAll(`.${styles['video-progress']} div`);
+        let videoVolprogress= this.dom.querySelectorAll(`.${styles['video-volprogress']} div`);
         let timer;
+
+        videoContent.volume = .5;
         //canpaly -->监听是否加载完毕
         videoContent.addEventListener('canplay',()=>{
             console.log('canplay');
@@ -129,11 +133,69 @@ class Video implements Icomponent{
             videoContent.requestFullscreen();
         });
 
+        //点击视频条拖拽播放
+        videoProgress[2].addEventListener('mousedown',function(ev:MouseEvent){         
+             let downX = ev.pageX;   
+             let downL = this.offsetLeft;   
+             document.onmousemove=(ev:MouseEvent)=>{
+                 //当前拖动的值-按下时的值 +距离+小圆球的宽度 除以父级
+                 let scale = (ev.pageX - downX + downL + 8)/this.parentNode.offsetWidth;
+                 if(scale<0){
+                     scale=0;
+                 }else if(scale>1){
+                    scale=1;
+                 }
+
+                 //设置移动的样式
+                 videoProgress[0].style.width = scale*100+'%';
+                 videoProgress[1].style.width = scale*100+'%';
+                 this.style.left = scale*100+'%';
+                 //设置当前拖拽后的播放时间 根据当前进度比例 乘以总时间 得出当前时间
+                 videoContent.currentTime = scale*videoContent.duration;
+             };
+             document.onmouseup = ()=>{
+                 document.onmousemove = document.onmouseup = null;
+             }
+             ev.preventDefault();
+        })
         
-        
+        videoVolprogress[1].addEventListener('mousedown',function(ev:MouseEvent){         
+            let downX = ev.pageX;   
+            let downL = this.offsetLeft;   
+            document.onmousemove=(ev:MouseEvent)=>{
+                //当前拖动的值-按下时的值 +距离+小圆球的宽度 除以父级
+                let scale = (ev.pageX - downX + downL + 8)/this.parentNode.offsetWidth;
+                if(scale<0){
+                    scale=0;
+                }else if(scale>1){
+                   scale=1;
+                }
+
+                //设置移动的样式
+                videoVolprogress[0].style.width = scale*100+'%';
+                this.style.left = scale*100+'%';
+                //设置当前拖拽后的音量
+                videoContent.volume = scale;
+            };
+            document.onmouseup = ()=>{
+                document.onmousemove = document.onmouseup = null;
+            }
+            ev.preventDefault();
+       })
+
+
+
+
         //正在播放进度
         function playing() {
+            //当前时间除以总时间 求出比例值
+            let scale = videoContent.currentTime/videoContent.duration;
+            //videoContent.buffered.end(0) 代表缓冲节点时间  求出scaleSuc缓存时间 比例
+            let scaleSuc = videoContent.buffered.end(0)/videoContent.duration;
             videoTimes[0].innerHTML = formatTime(videoContent.currentTime);
+            videoProgress[0].style.width = scale*100+'%';
+            videoProgress[1].style.width = scaleSuc*100+'%';
+            videoProgress[2].style.left = scale*100+'%';
         }
 
         function formatTime(number:number):string{
